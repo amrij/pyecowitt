ef27cd69ed954b2ae4948592923fb477c7ca0657
amrij <a.m.rijkeboer@am-rijkeboer.nl>
2024-10-25 13:02:46 +0200

    ecowitt

--- pyecowitt/__init__.py
+++ pyecowitt/__init__.py
@@ -1,23 +1,20 @@
 """ Nothing to see here """
 import sys
 
-__version__ = "0.10"
+__version__ = "0.7"
 
-__uri__ = 'https://github.com/garbled1/pyecowitt'
+__uri__ = 'https://github.com/amy/pyecowitt'
 __title__ = "pyecowitt"
 __description__ = 'Interface Library for Ecowitt Protocol'
 __doc__ = __description__ + " <" + __uri__ + ">"
-__author__ = 'Tim Rightnour'
-__email__ = 'root@garbled.net'
+__author__ = 'André Rijkeboer'
+__email__ = 'a.m.rijkeboer@am-rijkeboer.nl'
 __license__ = "Apache 2.0"
 
-__copyright__ = "Copyright (c) 2020 Tim Rightnour"
+__copyright__ = "Copyright (c) 2021 André Rijkeboer"
 
 from .ecowitt import (
     EcoWittListener,
-    WINDCHILL_OLD,
-    WINDCHILL_NEW,
-    WINDCHILL_HYBRID,
 )
 
 if __name__ == '__main__': print(__version__)
